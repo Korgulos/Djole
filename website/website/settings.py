69c270e39e4fc6ded34df0481abdcd995743c840
Dejan Katic <katicod100@gmail.com>
2023-04-20 09:08:40 +0200

    app index

--- website/website/settings.py
+++ website/website/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = "django-insecure-p!%pa+llc=8e1fqlh4@iv%h(8#(2p=$dbxk71bjd_b3r=7yrlb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'testsait.pythonanywhere.com',
+    '127.0.0.1',
+    ]
 
 
 # Application definition
@@ -42,10 +45,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # Extensions - installed with pip3 / requirements.txt
-    'bootstrap4',
-    "django_bootstrap5",
-    'bootstrap5',
-    'sass_processor',
+    #'bootstrap4',
+    #"django_bootstrap5",
+    #'bootstrap5',
+    #'sass_processor',
 
     # Djole's Applications
     "index.apps.IndexConfig",
@@ -81,80 +84,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "website.wsgi.application"
 
-# Default settings
-BOOTSTRAP4 = {
-
-    # The complete URL to the Bootstrap CSS file
-    # Note that a URL can be either a string,
-    # e.g. "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
-    # or a dict like the default value below.
-    "css_url": {
-        "href": "https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css",
-        "integrity": "sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn",
-        "crossorigin": "anonymous",
-    },
-
-    # The complete URL to the Bootstrap bundle JavaScript file
-    "javascript_url": {
-        "url": "https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js",
-        "integrity": "sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF",
-        "crossorigin": "anonymous",
-    },
-
-    # The complete URL to the Bootstrap CSS theme file (None means no theme)
-    "theme_url": None,
-
-    # The URL to the jQuery JavaScript file (full)
-    "jquery_url": {
-        "url": "https://code.jquery.com/jquery-3.5.1.min.js",
-        "integrity": "sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2",
-        "crossorigin": "anonymous",
-    },
-
-    # The URL to the jQuery JavaScript file (slim)
-    "jquery_slim_url": {
-        "url": "https://code.jquery.com/jquery-3.5.1.slim.min.js",
-        "integrity": "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj",
-        "crossorigin": "anonymous",
-    },
-
-    # Put JavaScript in the HEAD section of the HTML document (only relevant if you use bootstrap4.html)
-    'javascript_in_head': False,
-
-    # Include jQuery with Bootstrap JavaScript False|falsy|slim|full (default=False)
-    # False - means tag bootstrap_javascript use default value - `falsy` and does not include jQuery)
-    'include_jquery': False,
-
-    # Label class to use in horizontal forms
-    'horizontal_label_class': 'col-md-3',
-
-    # Field class to use in horizontal forms
-    'horizontal_field_class': 'col-md-9',
-
-    # Set placeholder attributes to label if no placeholder is provided
-    'set_placeholder': True,
-
-    # Class to indicate required (better to set this in your Django form)
-    'required_css_class': '',
-
-    # Class to indicate error (better to set this in your Django form)
-    'error_css_class': 'is-invalid',
-
-    # Class to indicate success, meaning the field has valid input (better to set this in your Django form)
-    'success_css_class': 'is-valid',
-
-    # Renderers (only set these if you have studied the source and understand the inner workings)
-    'formset_renderers': {
-        'default': 'bootstrap4.renderers.FormsetRenderer',
-    },
-    'form_renderers': {
-        'default': 'bootstrap4.renderers.FormRenderer',
-    },
-    'field_renderers': {
-        'default': 'bootstrap4.renderers.FieldRenderer',
-        'inline': 'bootstrap4.renderers.InlineFieldRenderer',
-    },
-}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -206,7 +135,7 @@ STATIC_URL = "static/"
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'sass_processor.finders.CssFinder',
+#    'sass_processor.finders.CssFinder',
 ]
 
 
